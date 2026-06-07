15bc298e90684e4d2f1f84a5ea80f1fe57da2924
haruki.ono <haruki.ono@optim.co.jp>
2026-06-07 16:10:14 +0900

    test: widget_testの修正 (ProviderScopeの追加と期待値の調整)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,36 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:subscription_manager/main.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:subscription_manager/providers/subscription_provider.dart';
+import 'package:subscription_manager/providers/payment_method_provider.dart';
+
+class MockSupabaseClient extends Mock implements SupabaseClient {}
+class MockGoTrueClient extends Mock implements GoTrueClient {}
 
 void main() {
-  testWidgets('Subscription Manager の画面が表示される', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-    expect(find.text('Subscription Manager'), findsOneWidget);
+  testWidgets('ログイン画面が表示される', (WidgetTester tester) async {
+    final mockSupabase = MockSupabaseClient();
+    final mockAuth = MockGoTrueClient();
+    when(() => mockSupabase.auth).thenReturn(mockAuth);
+    when(() => mockAuth.onAuthStateChange).thenAnswer(
+      (_) => Stream.value(AuthState(AuthChangeEvent.initialSession, null)),
+    );
+    when(() => mockAuth.currentUser).thenReturn(null);
+
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          supabaseProvider.overrideWithValue(mockSupabase),
+          subscriptionsProvider.overrideWith((ref) => const Stream.empty()),
+          paymentMethodsProvider.overrideWith((ref) => const Stream.empty()),
+        ],
+        child: const MyApp(),
+      ),
+    );
+    
+    await tester.pump();
+    expect(find.text('ログイン'), findsAtLeastNWidgets(1));
   });
 }
